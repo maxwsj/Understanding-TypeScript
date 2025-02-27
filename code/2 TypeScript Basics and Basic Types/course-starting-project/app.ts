25d8f7715e8740ddd5b29ddab8d03ee45fb6b852
maxwsj <mw2study@gmail.com>
2025-02-27 20:43:25 -0300

    add function types concepts

--- code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
+++ code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
@@ -1,3 +1,5 @@
+///// working with void
+
 function add(n1: number, n2: number) {
    return n1 + n2;
 }
@@ -7,3 +9,9 @@ function printResult(num: number) {
 }
 
 printResult(add(5, 12));
+
+///// function types
+let combineValues: (a: number, b: number) => number;
+combineValues = printResult;
+
+combineValues = add;
