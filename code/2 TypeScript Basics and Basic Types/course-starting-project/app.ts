53ccfb06bebbeb82ef805598f9791aaa23203fda
maxwsj <mw2study@gmail.com>
2025-02-26 23:00:57 -0300

    working with tuples

--- code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
+++ code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
@@ -10,3 +10,15 @@ console.log(person);
 ///// Arrays Types
 let favoriteActivities: string[];
 favoriteActivities = ['Sports', 'Video Games'];
+
+//// Working with Tuples
+
+const cars: {
+   name: string;
+   quantity: number;
+   role: [number, string];
+} = {
+   name: 'Mercedez',
+   quantity: 2,
+   role: [0, 'string'],
+};
