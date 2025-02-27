7a713b1edb6e2ab86e1885d77af73df0b94832de
maxwsj <mw2study@gmail.com>
2025-02-27 00:00:43 -0300

    add literal types logic

--- code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
+++ code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
@@ -1,5 +1,9 @@
 // Union Types
-function combine(input1: number | string, input2: number | string) {
+function combine(
+   input1: number | string,
+   input2: number | string,
+   resultConverion: string
+) {
    let result;
    if (typeof input1 === 'number' && typeof input2 === 'number') {
       result = input1 + input2;
@@ -9,8 +13,36 @@ function combine(input1: number | string, input2: number | string) {
    return result;
 }
 
-const combinedAges = combine(30, 26);
-const combinedNames = combine('Max', 'William');
+const combinedAges = combine(30, 26, 'as-number');
+const combinedNames = combine('Max', 'William', 'as-string');
 
 console.log(combinedAges);
 console.log(combinedNames);
+
+// Literal Types
+
+function combining(
+   input1: number | string,
+   input2: number | string,
+   resultConversion: string
+) {
+   let result;
+   if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2;
+   } else {
+      result = input1.toString() + input2.toString();
+   }
+   if (resultConversion === 'as-number') {
+      return +result;
+   } else {
+      return result.toString();
+   }
+}
+
+const combinedAge = combining(30, 26, 'as-number');
+const combinedName = combining('Max', 'William', 'as-string');
+const combinedNumber = combining('30', '20', 'as-string');
+
+console.log(combinedAge);
+console.log(combinedName);
+console.log(combinedNumber);
