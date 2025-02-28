6e0fb5107dfb2bed63723539a1cbc6a945afba09
maxwsj <mw2study@gmail.com>
2025-02-27 21:11:04 -0300

    add never type concept

--- code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
+++ code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
@@ -7,3 +7,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
    userName = userInput;
 }
+
+// The "never" type
+function generateError(message: string, code: number) {
+   throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
