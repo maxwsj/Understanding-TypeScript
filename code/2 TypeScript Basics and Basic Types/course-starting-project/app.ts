5760fc806a20863dab5c7e4dccc1ebf711a05881
maxwsj <mw2study@gmail.com>
2025-02-27 21:05:01 -0300

    add unknown type concept

--- code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
+++ code/2 TypeScript Basics and Basic Types/course-starting-project/app.ts
@@ -1,17 +1,9 @@
-///// working with void
+// The "unknown" type
+let userInput: unknown;
+let userName: string;
 
-function add(n1: number, n2: number) {
-   return n1 + n2;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+   userName = userInput;
 }
-
-function printResult(num: number) {
-   console.log('Result: ' + num);
-}
-
-printResult(add(5, 12));
-
-///// function types
-let combineValues: (a: number, b: number) => number;
-combineValues = printResult;
-
-combineValues = add;
