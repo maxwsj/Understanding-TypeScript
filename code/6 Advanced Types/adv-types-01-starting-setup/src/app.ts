5ce6d11bc1cd38d34c4412f3083a356b0c1ac85d
maxwsj <mw2study@gmail.com>
2025-03-02 19:26:45 -0300

    add more type guards concepts

--- code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
+++ code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
@@ -16,3 +16,18 @@ const e1: Elevatedemployee = {
    privileges: ['create-server'],
    startDate: new Date(),
 };
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+   console.log('Name: ' + emp.name);
+   if ('privileges' in emp) {
+      console.log('Privileges: ' + emp.privileges);
+   }
+
+   if ('startDate' in emp) {
+      console.log('Start Date: ' + emp.startDate);
+   }
+}
+
+printEmployeeInformation(e1);
