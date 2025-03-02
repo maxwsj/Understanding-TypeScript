9c3d982a198fa690e1f8ebce78d6ed128df7a597
maxwsj <mw2study@gmail.com>
2025-03-02 16:22:42 -0300

    add inheritance, overriding properties, protected modifier and getters and setters

--- code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,7 +2,7 @@
 class Department {
    // private id: string;
    // private name: string;
-   private empolyees: string[] = [];
+   protected empolyees: string[] = [];
 
    constructor(private readonly id: string, public name: string) {
       // this.name = n;
@@ -22,9 +22,62 @@ class Department {
    }
 }
 
-const accounting = new Department('d1', 'Accounting');
-accounting.addEmployee('Max');
-accounting.addEmployee('Andressa');
+class ItDepartment extends Department {
+   constructor(id: string, public admins: string[]) {
+      super(id, 'IT');
+   }
+}
 
-accounting.describe();
-accounting.printEmployeeInformation();
+class AccountingDepartment extends Department {
+   private lastReport: string;
+
+   get mostRecentReport() {
+      if (this.lastReport) {
+         return this.lastReport;
+      }
+      throw new Error('No report found.');
+   }
+
+   set mostRecentReport(value: string) {
+      if (!value) {
+         throw new Error('Please pass in a valid value!');
+      }
+      this.addReport(value);
+   }
+
+   constructor(id: string, private reports: string[]) {
+      super(id, 'Account');
+      this.lastReport = reports[0];
+   }
+
+   addEmployee(name: string) {
+      if (name === 'Max') {
+         return;
+      }
+      this.empolyees.push(name);
+   }
+
+   addReport(text: string) {
+      this.reports.push(text);
+      this.lastReport = text;
+   }
+
+   printReports() {
+      console.log(this.reports);
+   }
+}
+
+const it = new ItDepartment('d1', ['Max']);
+it.addEmployee('Max');
+it.addEmployee('Andressa');
+
+it.describe();
+it.printEmployeeInformation();
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.mostRecentReport = 'Year End Report';
+
+accounting.addReport('Something went wrong...');
+
+accounting.printReports();
