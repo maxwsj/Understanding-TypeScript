587c5ff69581247b939115951db086f60dcec931
maxwsj <mw2study@gmail.com>
2025-03-02 20:42:34 -0300

    add optional chaining

--- code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
+++ code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
@@ -76,3 +76,12 @@ interface ErrorContainer {
 const errorBag: ErrorContainer = {
    email: 'Not a valid email',
 };
+
+////////// Optional Chaining
+const fetchedUserData = {
+   id: 'u1',
+   name: 'Max',
+   job: { title: 'CEO', description: 'My own company' },
+};
+
+console.log(fetchedUserData?.job?.title);
