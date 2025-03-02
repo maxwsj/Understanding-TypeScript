c4f65887ea29a2eef0c083cb768cb260c5006353
maxwsj <mw2study@gmail.com>
2025-03-02 19:03:08 -0300

    add optional parameters and properties

--- code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,5 +1,6 @@
 interface Named {
-   readonly name: string;
+   readonly name?: string;
+   outputName?: string;
 }
 
 interface AddFn {
@@ -11,20 +12,26 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-   name: string;
+   name?: string;
    age = 30;
 
-   constructor(n: string) {
-      this.name = n;
+   constructor(n?: string) {
+      if (n) {
+         this.name = n;
+      }
    }
 
    greet(phrase: string) {
-      console.log(phrase + ' ' + this.name);
+      if (this.name) {
+         console.log(phrase + ' ' + this.name);
+      } else {
+         console.log('Hi');
+      }
    }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person();
 
 user1.greet('Hi There');
