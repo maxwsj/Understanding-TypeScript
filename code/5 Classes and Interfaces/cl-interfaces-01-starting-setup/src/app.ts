7e47bdb7a65326546a3814a290ec7a8544e99f34
maxwsj <mw2study@gmail.com>
2025-03-02 16:00:23 -0300

    add readonly concepts

--- code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,15 +1,30 @@
 // Code goes here!
 class Department {
-   name: string;
+   // private id: string;
+   // private name: string;
+   private empolyees: string[] = [];
 
-   constructor(n: string) {
-      this.name = n;
+   constructor(private readonly id: string, public name: string) {
+      // this.name = n;
    }
 
    describe(this: Department) {
-      console.log('Department: ' + this.name);
+      console.log(`Department (${this.id}): ${this.name}`);
+   }
+
+   addEmployee(employee: string) {
+      this.empolyees.push(employee);
+   }
+
+   printEmployeeInformation() {
+      console.log(this.empolyees.length);
+      console.log(this.empolyees);
    }
 }
 
-const accounting = new Department('Accounting');
-console.log(accounting);
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Andressa');
+
+accounting.describe();
+accounting.printEmployeeInformation();
