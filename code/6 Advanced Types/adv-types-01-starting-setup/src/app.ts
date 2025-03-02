641ca3673fb0d96d4334f0783a76528987587e75
maxwsj <mw2study@gmail.com>
2025-03-02 19:18:40 -0300

    add intersection types

--- code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
+++ code/6 Advanced Types/adv-types-01-starting-setup/src/app.ts
@@ -1,1 +1,18 @@
 // Code goes here!
+type Admin = {
+   name: string;
+   privileges: string[];
+};
+
+type Employee = {
+   name: string;
+   startDate: Date;
+};
+
+type Elevatedemployee = Admin & Employee;
+
+const e1: Elevatedemployee = {
+   name: 'Max',
+   privileges: ['create-server'],
+   startDate: new Date(),
+};
