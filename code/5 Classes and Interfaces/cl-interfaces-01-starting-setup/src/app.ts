26e1426380e228087ed27e220c64ff63c6ae9856
maxwsj <mw2study@gmail.com>
2025-03-02 14:16:17 -0300

    add constructor functions and the 'this' keyword concept code

--- code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ code/5 Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -5,6 +5,10 @@ class Department {
    constructor(n: string) {
       this.name = n;
    }
+
+   describe(this: Department) {
+      console.log('Department: ' + this.name);
+   }
 }
 
 const accounting = new Department('Accounting');
