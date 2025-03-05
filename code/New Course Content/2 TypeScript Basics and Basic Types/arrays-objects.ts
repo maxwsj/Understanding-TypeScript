f1ed59dea49193aed439c4cf08a38713e2798b3e
maxwsj <mw2study@gmail.com>
2025-03-04 23:36:24 -0300

    add arrays generic type declaration

--- code/New Course Content/2 TypeScript Basics and Basic Types/arrays-objects.ts
+++ code/New Course Content/2 TypeScript Basics and Basic Types/arrays-objects.ts
@@ -1,5 +1,13 @@
+// Advanced array types
 let users: (string | number)[];
 
 users = [1, 'Max'];
 users = [5, 1];
 users = ['Max', 'Andressa'];
+
+// Generic Types - alternative array type declaration
+let person: Array<string | number>;
+
+person = [1, 'Max'];
+person = [5, 1];
+person = ['Max', 'Andressa'];
