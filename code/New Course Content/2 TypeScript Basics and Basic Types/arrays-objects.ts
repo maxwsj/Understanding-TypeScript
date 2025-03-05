38c4d4312bc3e33d3709e084889be72af2c3b25e
maxwsj <mw2study@gmail.com>
2025-03-04 23:59:02 -0300

    add enums concepts

--- code/New Course Content/2 TypeScript Basics and Basic Types/arrays-objects.ts
+++ code/New Course Content/2 TypeScript Basics and Basic Types/arrays-objects.ts
@@ -36,31 +36,3 @@ let data: Record<string, number | string>;
 data = {
    entry1: 1,
 };
-
-///////// Working with Enums
-
-///////// Beign specific with literal types
-
-///////// Types Aliases & custom types
-
-///////// Function return value types
-
-///////// The “void” type
-
-///////// The “never” type
-
-///////// Functions as types
-
-///////// null & undefined - special types
-
-///////// Inferred null & a first look at type narrowing
-
-///////// Forced “Not Null” and optional chaining
-
-///////// Type casting
-
-///////// The “unknown” type
-
-///////// Optional values & typescript
-
-///////// Nullish coalescing
---
+++ code/New Course Content/2 TypeScript Basics and Basic Types/more-basic-types.ts
@@ -0,0 +1,36 @@
+///////// Working with Enums
+enum Role {
+   Admin,
+   Editor,
+   Guest,
+}
+
+let userRole: Role = Role.Admin;
+
+let userR: Role = 2;
+
+///////// Beign specific with literal types
+
+///////// Types Aliases & custom types
+
+///////// Function return value types
+
+///////// The “void” type
+
+///////// The “never” type
+
+///////// Functions as types
+
+///////// null & undefined - special types
+
+///////// Inferred null & a first look at type narrowing
+
+///////// Forced “Not Null” and optional chaining
+
+///////// Type casting
+
+///////// The “unknown” type
+
+///////// Optional values & typescript
+
+///////// Nullish coalescing
