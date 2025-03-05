6ee6f20cafa95c9016115ab0e2e2c17bc81908cf
maxwsj <mw2study@gmail.com>
2025-03-05 01:19:26 -0300

    add application logic for the calculateInvestment function

--- code/New Course Content/3 TypeScript Essentials Demo Project/calculator.ts
+++ code/New Course Content/3 TypeScript Essentials Demo Project/calculator.ts
@@ -20,7 +20,42 @@ type InvestmentResult = {
 
 type CalculationResult = InvestmentResult[] | string;
 
-function calculateInvestment(data: InvestmentData): CalculationResult {} // => result[]
+function calculateInvestment(data: InvestmentData): CalculationResult {
+   const { initialAmount, annualContribution, expectedReturn, duration } = data;
+
+   if (initialAmount < 0) {
+      return 'Initial investment amount must be at least zero';
+   }
+
+   if (duration <= 0) {
+      return 'No valid amount of years provided.';
+   }
+
+   if (expectedReturn <= 0) {
+      return 'Expected return ust be at least zero';
+   }
+
+   let total = initialAmount;
+   let totalContributions = 0;
+   let totalInterestEarned = 0;
+
+   const annualResults: InvestmentResult[] = [];
+
+   for (let i = 0; i < duration; i++) {
+      total = total * (1 + expectedReturn);
+      totalInterestEarned = total - totalContributions - initialAmount;
+      totalContributions = totalContributions + annualContribution;
+      total = total + annualContribution;
+
+      annualResults.push({
+         year: `Year ${i + 1}`,
+         totalAmount: total,
+         totalInterestEarned,
+         totalContributions,
+      });
+   }
+   return annualResults;
+} // => result[]
 
 function printResults(results: number[]) {
    // print output the result data
