d3b8ead0681813c6d082938545e17a1aeeeee145
maxwsj <mw2study@gmail.com>
2025-03-05 01:01:07 -0300

    setting up the initial project

---
+++ code/New Course Content/3 TypeScript Essentials Demo Project/calculator.ts
@@ -0,0 +1,15 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data: number[]) {} // => result[]
+
+function printResults(results: number[]) {
+   // print output the result data
+}
+
+const results = calculateInvestment([1, 2, 3]);
+
+printResults(results);
