79b1544830465dd9b688bced6d82781139cb7c23
maxwsj <mw2study@gmail.com>
2025-03-07 23:21:13 -0300

    make the class generic

--- code/New Course Content/9 Classes and Generics - Demo Project/linked-list.js
+++ code/New Course Content/9 Classes and Generics - Demo Project/linked-list.js
@@ -4,4 +4,7 @@ class ListNode {
 class LinkedList {
     root;
     length = 0;
+    add(value) { }
 }
+const numberList = new LinkedList();
+const stringList = new LinkedList();
--- code/New Course Content/9 Classes and Generics - Demo Project/linked-list.ts
+++ code/New Course Content/9 Classes and Generics - Demo Project/linked-list.ts
@@ -1,6 +1,11 @@
 class ListNode {}
 
-class LinkedList {
+class LinkedList<T> {
    private root?: ListNode;
    private length = 0;
+
+   add(value: T) {}
 }
+
+const numberList = new LinkedList<number>();
+const stringList = new LinkedList<string>();
