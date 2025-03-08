934a59f5340eb38a798a4bd3e670b2881c959e2e
maxwsj <mw2study@gmail.com>
2025-03-07 23:30:27 -0300

    add an 'add' method

--- code/New Course Content/9 Classes and Generics - Demo Project/linked-list.js
+++ code/New Course Content/9 Classes and Generics - Demo Project/linked-list.js
@@ -1,10 +1,28 @@
 "use strict";
 class ListNode {
+    value;
+    next;
+    constructor(value) {
+        this.value = value;
+    }
 }
 class LinkedList {
     root;
     length = 0;
-    add(value) { }
+    add(value) {
+        const node = new ListNode(value);
+        if (!this.root) {
+            this.root = node;
+        }
+        else {
+            let current = this.root;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.length++;
+    }
 }
 const numberList = new LinkedList();
 const stringList = new LinkedList();
