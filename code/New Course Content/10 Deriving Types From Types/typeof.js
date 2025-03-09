5ed32eacd5cb579782612bc2f51fe59d748ea38d
maxwsj <mw2study@gmail.com>
2025-03-09 17:03:47 -0300

    add typeof usage and more useful examples

--- code/New Course Content/10 Deriving Types From Types/typeof.js
+++ code/New Course Content/10 Deriving Types From Types/typeof.js
@@ -1,2 +1,16 @@
 "use strict";
+////////////// "typeof" & A More Useful Example
 const userName = 'Max';
+const settings = {
+    diffuculty: 'easy',
+    minLevel: 10,
+    didStart: false,
+    player: ['Jhon', 'Jane'],
+};
+function loadData(s) {
+    //....
+}
+// or
+function data(s) {
+    //....
+}
--- code/New Course Content/10 Deriving Types From Types/typeof.ts
+++ code/New Course Content/10 Deriving Types From Types/typeof.ts
@@ -1,3 +1,23 @@
+////////////// "typeof" & A More Useful Example
+
 const userName = 'Max';
 
 type UserName = typeof userName;
+
+const settings = {
+   diffuculty: 'easy',
+   minLevel: 10,
+   didStart: false,
+   player: ['Jhon', 'Jane'],
+};
+
+type Settings = typeof settings;
+
+function loadData(s: Settings) {
+   //....
+}
+
+// or
+function data(s: typeof settings) {
+   //....
+}
