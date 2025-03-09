e3e4cb7b5f267437a3dd90aeff37eb4f37a67116
maxwsj <mw2study@gmail.com>
2025-03-09 17:26:58 -0300

    add more 'keyof' concepts and a more useful example

--- code/New Course Content/10 Deriving Types From Types/keyof.js
+++ code/New Course Content/10 Deriving Types From Types/keyof.js
@@ -1,3 +1,15 @@
 "use strict";
 let validKey;
 validKey = 'age';
+/////////// "keyof" & a more useful example
+function getProp(obj, key) {
+    const val = obj[key];
+    if (val === undefined || val === null) {
+        throw new Error('Accesing undefined or null value');
+    }
+    return val;
+}
+const user = { name: 'Max', age: 35 };
+const val = getProp(user, 'age');
+const nameValue = getProp(user, 'name'); // ✅ Type inferred as string
+const ageValue = getProp(user, 'age'); // ✅ Type inferred as number
--- code/New Course Content/10 Deriving Types From Types/keyof.ts
+++ code/New Course Content/10 Deriving Types From Types/keyof.ts
@@ -5,3 +5,19 @@ type UserKeys = keyof User;
 let validKey: UserKeys;
 
 validKey = 'age';
+
+/////////// "keyof" & a more useful example
+function getProp<T extends object, U extends keyof T>(obj: T, key: U) {
+   const val = obj[key];
+   if (val === undefined || val === null) {
+      throw new Error('Accesing undefined or null value');
+   }
+
+   return val;
+}
+
+const user = { name: 'Max', age: 35 };
+
+const val = getProp(user, 'age');
+const nameValue = getProp(user, 'name'); // ✅ Type inferred as string
+const ageValue = getProp(user, 'age'); // ✅ Type inferred as number
