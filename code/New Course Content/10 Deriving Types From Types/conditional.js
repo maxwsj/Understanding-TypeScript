6f2508b495ee4501c13be1ed3244775fb7a9f69d
maxwsj <mw2study@gmail.com>
2025-03-09 23:31:18 -0300

    add another example of conditional types

--- code/New Course Content/10 Deriving Types From Types/conditional.js
+++ code/New Course Content/10 Deriving Types From Types/conditional.js
@@ -1,5 +1,15 @@
 "use strict";
 let text = 1;
-//////////// Conditional types - another example
+function getFullname(person) {
+    if ('firstName' in person &&
+        'lastName' in person &&
+        person.firstName &&
+        person.lastName) {
+        return `${person.firstName} ${person.lastName}`;
+    }
+    throw new Error('No first name and / or last name found.');
+}
+const name1 = getFullname({});
+const name2 = getFullname({ firstName: 'Max', lastName: 'William' });
 //////////// Making sense of the “infer” leyword
 //////////// Typescript’s got you covered: built-in utility types
--- code/New Course Content/10 Deriving Types From Types/conditional.ts
+++ code/New Course Content/10 Deriving Types From Types/conditional.ts
@@ -10,6 +10,25 @@ type Example2 = GetElementType<typeof text>;
 
 //////////// Conditional types - another example
 
+type FullnamePerson = { firstName: string; lastName: string };
+type FullnameOrNothing<T> = T extends FullnamePerson ? string : never;
+
+function getFullname<T extends object>(person: T): FullnameOrNothing<T> {
+   if (
+      'firstName' in person &&
+      'lastName' in person &&
+      person.firstName &&
+      person.lastName
+   ) {
+      return `${person.firstName} ${person.lastName}` as FullnameOrNothing<T>;
+   }
+
+   throw new Error('No first name and / or last name found.');
+}
+
+const name1 = getFullname({});
+const name2 = getFullname({ firstName: 'Max', lastName: 'William' });
+
 //////////// Making sense of the “infer” leyword
 
 //////////// Typescript’s got you covered: built-in utility types
