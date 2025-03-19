872f59255d93db917587220805bc49910bcd8dfc
maxwsj <mw2study@gmail.com>
2025-03-18 21:22:05 -0300

    adding multiple decorators

--- code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
+++ code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
@@ -7,6 +7,7 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
    return function (constructor: any) {
+      console.log('Rendering template');
       const hookEl = document.getElementById(hookId);
       const p = new constructor();
       if (hookEl) {
@@ -16,7 +17,7 @@ function WithTemplate(template: string, hookId: string) {
    };
 }
 
-// @Logger('LOGGIN - PERSON')
+@Logger('LOGGIN - PERSON')
 @WithTemplate('<h1>My Personal Object</h1>', 'app')
 class Person {
    name = 'Max';
