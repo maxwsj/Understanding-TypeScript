8f36da06006f9780dd7b2e761b07d0c43fe26ed8
maxwsj <mw2study@gmail.com>
2025-03-18 23:09:00 -0300

    validation with decorators - first steps

--- code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
+++ code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
@@ -121,3 +121,36 @@ const p = new Printer();
 
 const button = document.querySelector('button')!;
 button.addEventListener('click', p.showMessage);
+
+/////// Validation with decorators - first steps
+
+function Required() {}
+
+function PositiveNumber() {}
+
+function validate(obj: object) {}
+
+class Course {
+   @Required
+   title: string;
+   @PositiveNumber
+   price: number;
+
+   constructor(t: string, p: number) {
+      this.title = t;
+      this.price = p;
+   }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (event) => {
+   event.preventDefault();
+   const titleEl = document.getElementById('title') as HTMLInputElement;
+   const priceEl = document.getElementById('price') as HTMLInputElement;
+
+   const title = titleEl.value;
+   const price = +priceEl.value;
+
+   const createdCourse = new Course(title, price);
+   console.log(createdCourse);
+});
