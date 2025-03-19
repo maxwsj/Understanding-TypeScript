2d3da4602ab049cd4e51c34e5ce5a8798ad3510b
maxwsj <mw2study@gmail.com>
2025-03-18 22:57:43 -0300

    example creating an autobind decorator

--- code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
+++ code/New Course Content/12 Experimental Decorators/decorators-01-starting-setup/src/app.ts
@@ -34,7 +34,7 @@ class Person {
    }
 }
 
-const p = new Person();
+const per = new Person();
 
 ///// Diving into Property Decorators
 
@@ -92,3 +92,32 @@ class Product {
       return this._price * (1 + tax);
    }
 }
+
+//// Example: Creating an "Autobind" Decorator
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+   const originalMethod = descriptor.value;
+   const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: false,
+      get() {
+         const boundFn = originalMethod.bind(this);
+         return boundFn;
+      },
+   };
+   return adjDescriptor;
+}
+
+class Printer {
+   message = 'This works!';
+
+   @Autobind
+   showMessage() {
+      console.log(this.message);
+   }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
