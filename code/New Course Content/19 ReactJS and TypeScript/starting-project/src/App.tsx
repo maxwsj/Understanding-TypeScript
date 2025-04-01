8bc6148ec32daea279f22e5ce141f81b559f25ae
maxwsj <mw2study@gmail.com>
2025-03-31 22:58:35 -0300

    managing state

--- code/New Course Content/19 ReactJS and TypeScript/starting-project/src/App.tsx
+++ code/New Course Content/19 ReactJS and TypeScript/starting-project/src/App.tsx
@@ -1,27 +1,27 @@
 import Header from './components/Header';
 import CourseGoals from './components/CourseGoals';
 import goalsImg from './assets/goals.jpg';
+import { useState } from 'react';
 
 function App() {
+   const [goals, setGoals] = useState([
+      {
+         id: 1,
+         title: 'Learn TS',
+         description: 'Learn TS from the ground up',
+      },
+      {
+         id: 2,
+         title: 'Practice TS',
+         description: 'Practice working with TS',
+      },
+   ]);
    return (
       <main>
          <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
             <h1>Your course goals</h1>
          </Header>
-         <CourseGoals
-            goals={[
-               {
-                  id: 1,
-                  title: 'Learn TS',
-                  description: 'Learn TS from the ground up',
-               },
-               {
-                  id: 2,
-                  title: 'Practice TS',
-                  description: 'Practice working with TS',
-               },
-            ]}
-         />
+         <CourseGoals goals={goals} />
       </main>
    );
 }
