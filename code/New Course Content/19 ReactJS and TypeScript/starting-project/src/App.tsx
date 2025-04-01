00d880736567a823099570eff6c10e367d3fec57
maxwsj <mw2study@gmail.com>
2025-03-31 22:31:00 -0300

    the special children prop and optional props

--- code/New Course Content/19 ReactJS and TypeScript/starting-project/src/App.tsx
+++ code/New Course Content/19 ReactJS and TypeScript/starting-project/src/App.tsx
@@ -4,7 +4,9 @@ import goalsImg from './assets/goals.jpg';
 function App() {
    return (
       <main>
-         <Header image={{ src: goalsImg, alt: 'A list of goals' }} />
+         <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+            <h1>Your course goals</h1>
+         </Header>
       </main>
    );
 }
